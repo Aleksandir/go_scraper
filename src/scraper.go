db03064d746669bb6ff81234e9402b25acfb82df
Aleks B <aleksandir@me.com>
2023-12-21 19:53:35 +1030

    Refactor code to separate functions for writing products to CSV and getting number of pages

--- src/scraper.go
+++ src/scraper.go
@@ -33,50 +33,14 @@ func main() {
 	})
 
 	c.OnScraped(func(r *colly.Response) {
-		file, err := os.Create("products.csv")
-		if err != nil {
-			log.Fatalln("Failed to create output CSV file", err)
-		}
-		defer file.Close()
-
-		writer := csv.NewWriter(file)
-
-		headers := []string{
-			"url",
-			"image",
-			"name",
-			"price",
-		}
-		writer.Write(headers)
-
-		for _, pokemonProduct := range pokemonProducts {
-			record := []string{
-				pokemonProduct.url,
-				pokemonProduct.image,
-				pokemonProduct.name,
-				pokemonProduct.price,
-			}
-
-			writer.Write(record)
-		}
-
-		writer.Flush()
+		writeProductsToCSV(pokemonProducts)
 	})
 
 	// Start scraping on https://scrapeme.live/shop/page/1 through to https://scrapeme.live/shop/page/48
 	// concurrently using goroutines and a WaitGroup
 	var wg sync.WaitGroup
 
-	// Create a new collector for getting the max number of pages
-	c1 := colly.NewCollector()
-
-	var numPages int
-
-	c1.OnHTML("ul.page-numbers li:nth-last-child(2) a", func(e *colly.HTMLElement) {
-		numPages, _ = strconv.Atoi(e.Text)
-	})
-
-	c1.Visit("https://scrapeme.live/shop/page/1")
+	numPages := getNumPages("https://scrapeme.live/shop/page/1")
 
 	println("Number of pages: ", numPages)
 
@@ -98,4 +62,50 @@ func main() {
 
 	// Finish the progress bar
 	bar.Finish()
+}
+
+func getNumPages(url string) int {
+	// Create a new collector for getting the max number of pages
+	c1 := colly.NewCollector()
+
+	var numPages int
+
+	c1.OnHTML("ul.page-numbers li:nth-last-child(2) a", func(e *colly.HTMLElement) {
+		numPages, _ = strconv.Atoi(e.Text)
+	})
+
+	c1.Visit(url)
+
+	return numPages
+}
+
+func writeProductsToCSV(pokemonProducts []PokemonProduct) {
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+	writer.Write(headers)
+
+	for _, pokemonProduct := range pokemonProducts {
+		record := []string{
+			pokemonProduct.url,
+			pokemonProduct.image,
+			pokemonProduct.name,
+			pokemonProduct.price,
+		}
+
+		writer.Write(record)
+	}
+
+	writer.Flush()
 }
